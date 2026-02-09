af7c4e9b8dee70d11624cf3f37d7f1e0a6ee9810
강희진 <rkd8527@naver.com>
2026-02-09 17:45:24 +0900

    Initial commit

---
+++ src/main/kotlin/com/khj/ragdocument/RagDocumentApplication.kt
@@ -0,0 +1,11 @@
+package com.khj.ragdocument
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RagDocumentApplication
+
+fun main(args: Array<String>) {
+    runApplication<RagDocumentApplication>(*args)
+}
